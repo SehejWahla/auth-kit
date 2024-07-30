d7bc8985f94a88be9539801dba04df122bf29d50
Sehej Wahla <sehej@sendatradie.com>
2024-07-31 02:52:54 +1000

    zod validation

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,5 +1,19 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import {Form, FormControl, FormField, FormItem, FormLabel, FormMessage} from "@/components/ui/form";
+import { loginSchema } from "@/schemas";
+
 import CardWrapper from "./card-wrapper";
 const LoginForm = () => {
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver : zodResolver(loginSchema),
+        defaultValues : {
+            email : "",
+            password : ""
+        }
+    });
     return <div><CardWrapper headerLabel="Welcome!" backButtonLabel="Don't have an account?" backButtonHref="/auth/register" showSocial>Login Form!</CardWrapper></div>
 }
 
